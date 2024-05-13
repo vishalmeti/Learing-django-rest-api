8ece75df64c45449b0d3009b7fac06542bd7841e
vishalmeti <vishalmeti9@gmail.com>
2024-05-13 14:24:52 +0530

    reading param value from url into api function

--- rest/base/views.py
+++ rest/base/views.py
@@ -7,11 +7,16 @@ from rest_framework.response import Response
 @api_view(['GET','POST'])
 def hello_world(request):
     if request.method == 'GET':
+        # to get the params from the url, we need to name the param into get function as a string
+        paramData = request.GET.get('name')
+        print("---------")
+        print('Param :',paramData)
+        print("---------")
         return Response({"message": "You hit GET method: Hello, world!"})
     elif request.method == 'POST':
         data = request.data
         print("-----------")
-        print(data)
+        print('Request Data :',data)
         print("-----------")
         return Response({"message": "You hit POST method: Hello, world!",
                          "data":data})
