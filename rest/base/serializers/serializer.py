d7b9a90c589db9ce1df745a005864b510ad9e09c
vishalmeti <vishalmeti9@gmail.com>
2024-05-17 20:46:23 +0530

    added name validation in serializer

--- rest/base/serializers/serializer.py
+++ rest/base/serializers/serializer.py
@@ -6,3 +6,9 @@ class PersonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Person
         fields = '__all__'
+    
+    def validate(self, data):
+        if any(char.isdigit() for char in data['name']):
+            raise serializers.ValidationError({'error':'Name should not contain numbers'})
+        
+        return data
