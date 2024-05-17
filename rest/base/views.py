0ac2c635b7a1c8af9174080f3e9026c8396c4039
vishalmeti <vishalmeti9@gmail.com>
2024-05-17 18:44:54 +0530

    getting data from db and saving data into db

--- rest/base/views.py
+++ rest/base/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from base.models import Person
+from base.serializers import serializer
 
 # Create your views here.
 from rest_framework.decorators import api_view
@@ -9,14 +11,23 @@ def hello_world(request):
     if request.method == 'GET':
         # to get the params from the url, we need to name the param into get function as a string
         paramData = request.GET.get('name')
+        allPerson = Person.objects.all()
+        data=serializer.PersonSerializer(allPerson, many=True).data
+        
+        
         print("---------")
         print('Param :',paramData)
         print("---------")
-        return Response({"message": "You hit GET method: Hello, world!"})
+        return Response({"message": "You hit GET method: Hello, world!", "data": data})
     elif request.method == 'POST':
-        data = request.data
+        payload = request.data
+        serial = serializer.PersonSerializer(data=payload)
+        if not serial.is_valid():
+            return Response({"message":"Something went wrong", "error": serial.errors})
+        
+        serial.save()
         print("-----------")
-        print('Request Data :',data)
+        print('Request Data :',payload)
         print("-----------")
         return Response({"message": "You hit POST method: Hello, world!",
-                         "data":data})
+                         "data":payload})
