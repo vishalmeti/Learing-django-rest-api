e58d5d2412ff3e07bf63ec0dd4b65cc1ff3b80b1
vishalmeti <vishalmeti9@gmail.com>
2024-05-17 17:09:49 +0530

    added model and api routes

--- rest/api/urls.py
+++ rest/api/urls.py
@@ -2,5 +2,5 @@ from base.views import hello_world
 from django.urls import path , include
 
 urlpatterns = [
-    path('v1/',hello_world),
+    path('',hello_world),
 ]
--- rest/base/models.py
+++ rest/base/models.py
@@ -1,3 +1,10 @@
+# myapp/models.py
+
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField(default=18)
+    
+    def __str__(self):
+        return self.name
