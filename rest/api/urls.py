771166cab8cd70461b9892f5a33ae19f1fb4e308
vishalmeti <vishalmeti9@gmail.com>
2024-05-23 12:16:33 +0530

    API view Class || Moved users api methods into student api view class. Made a single url to do all the get put patch and delete.

--- rest/api/urls.py
+++ rest/api/urls.py
@@ -1,8 +1,10 @@
-from base.views import users , updateUser, getBooks
+# from base.views import users , updateUser, getBooks
+from base.views import getBooks , StudentAPI
 from django.urls import path , include
 
 urlpatterns = [
-    path('',users),
-    path('update/<int:id>',updateUser),
+    # path('',users),
+    path('',StudentAPI.as_view()),
+    path('update/<int:id>',StudentAPI.as_view()),
     path('book',getBooks)
 ]
--- rest/base/views.py
+++ rest/base/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from base.models import Student, Book
 from base.serializers import serializer
+from rest_framework.views import APIView
 
 # Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET','POST'])
-def users(request):
-    if request.method == 'GET':
+
+class StudentAPI(APIView):
+    def get(self, request):
         # to get the params from the url, we need to name the param into get function as a string
         paramData = request.GET.get('name')
         paramId = request.GET.get('id')
@@ -30,27 +31,23 @@ def users(request):
         print('Param :',paramData)
         print("---------")
         return Response({"data": data})
-    elif request.method == 'POST':
+    
+    def post(self,request):
         payload = request.data
         serial = serializer.StudentSerializer(data=payload)
         if not serial.is_valid():
             return Response({"message":"Something went wrong", "error": serial.errors})
         
         serial.save()
-        print("-----------")
         print('Request Data :',payload)
-        print("-----------")
         return Response({"message": "Data added","data":payload})
     
-@api_view(['PATCH','DELETE'])
-def updateUser(request, id):
-    print(request.data)
-    try:
-        student = Student.objects.get(id = id)
-    except:
-        return Response({"msg":"No Student found with id: "+str(id)})
+    def patch(self,request, id):
+        try:
+            student = Student.objects.get(id = id)
+        except:
+            return Response({"msg":"No Student found with id: "+str(id)})
      
-    if request.method == 'PATCH':   
         ser = serializer.StudentSerializer(student, data = request.data, partial = True)
         if not ser.is_valid():
             return Response({"error":ser.errors})
@@ -58,9 +55,77 @@ def updateUser(request, id):
         ser.save()
         return Response({"Student updated":ser.data})
     
-    elif request.method == 'DELETE':
+    def put(self,request):
+        pass
+    
+    def delete(self,request, id):
+        try:
+            student = Student.objects.get(id = id)
+        except:
+            return Response({"msg":"No Student found with id: "+str(id)})
+            
+        stud=serializer.StudentSerializer(student).data
+
         student.delete()
-        return Response({"msg":"Deleted"})   
+        return Response({"msg":"Deleted","student":stud})   
+        
+        
+
+# @api_view(['GET','POST'])
+# def users(request):
+#     if request.method == 'GET':
+#         # to get the params from the url, we need to name the param into get function as a string
+#         paramData = request.GET.get('name')
+#         paramId = request.GET.get('id')
+#         data=''
+#         if paramData or paramId:
+#             try:
+#                 # student = Student.objects.get(name = paramData)
+#                 student = Student.objects.get(id = paramId)
+#                 data = serializer.StudentSerializer(student).data
+#             except:
+#                 return Response({"msg":"No Student found with id: "+str(paramId)})
+
+#         else:
+#             allStudent = Student.objects.all()
+#             data=serializer.StudentSerializer(allStudent, many=True).data
+        
+        
+#         print("---------")
+#         print('Param :',paramData)
+#         print("---------")
+#         return Response({"data": data})
+#     elif request.method == 'POST':
+#         payload = request.data
+#         serial = serializer.StudentSerializer(data=payload)
+#         if not serial.is_valid():
+#             return Response({"message":"Something went wrong", "error": serial.errors})
+        
+#         serial.save()
+#         print("-----------")
+#         print('Request Data :',payload)
+#         print("-----------")
+#         return Response({"message": "Data added","data":payload})
+    
+# @api_view(['PATCH','DELETE'])
+# def updateUser(request, id):
+#     print(request.data)
+#     try:
+#         student = Student.objects.get(id = id)
+#     except:
+#         return Response({"msg":"No Student found with id: "+str(id)})
+     
+#     if request.method == 'PATCH':   
+#         ser = serializer.StudentSerializer(student, data = request.data, partial = True)
+#         if not ser.is_valid():
+#             return Response({"error":ser.errors})
+        
+#         ser.save()
+#         return Response({"Student updated":ser.data})
+    
+#     elif request.method == 'DELETE':
+#         student.delete()
+#         return Response({"msg":"Deleted"})   
     
 
 @api_view(['GET'])
